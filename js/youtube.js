c1b5a811fdef83ec04eaa8fe8fb1cfee77a4a1b2
YoungMin <rhenfla@gmail.com>
2021-11-15 16:47:19 +0900

    Start project

---
+++ js/youtube.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// 2. This code loads the IFrame Player API code asynchronously.
+let tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+
+function onYouTubeIframeAPIReady() {
+  new YT.Player('player', {
+    videoId: 'An6LvWQuj_8', // 최초 재생할 유튜브 영상 ID
+    playerVars: { // 영상을 재생하기 위한 여러가지 변수 옵션
+      autoplay: true, // 자동 재생 유무
+      loop: true, // 반복 재생 유무
+      playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID 목록
+    },
+    events: {
+      onReady: function (event) { // 플레이어가 준비가 되면 함수 실행
+        event.target.mute()  // target -재생되고 있는 영상 자체, mute - 음소거 
+      }
+    }
+  });
+}
+
